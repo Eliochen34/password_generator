f0fcb3665a8bee55df540e982a0b358aa254b879
Elio <eliochen34@gmail.com>
2022-10-03 16:25:52 +0800

    fullfill the password generate and show on the screen

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+// const bodyParser = require('body-parser') 此寫法已過時
+const generatePassword = require('./generate_password')
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// setting body-parser
+// app.use(bodyParser.urlencoded({ extended: true })) 此寫法已過時
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('random password is:', generatePassword(req.body))
+  const password = generatePassword(req.body)
+  res.render('index', { password: password })
 })
 
 app.listen(port, () => {
---
+++ generate_password.js
@@ -0,0 +1,67 @@
+// define sample function to randomly return a item in an array
+function sample(array){
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+
+// define generatePassword function
+function generatePassword(options){
+  
+  // define the things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@#$%^&*()-_+={}[]|;"<>,.?/'
+  // define dummy data
+  // const options = {
+  //   length: 12,
+  //   lowercase: 'on',
+  //   uppercase: 'on',
+  //   numbers: 'on',
+  //   excludeCharacters: '40',
+  // }
+  // console.log(options)
+  // create a collection to store things user picked up
+  let collection = []
+  if (options.lowercase === 'on'){
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+  if (options.uppercase === 'on'){
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+  if (options.numbers === 'on'){
+    collection = collection.concat(numbers.split(''))
+  }
+  if (options.symbols === 'on'){
+    collection = collection.concat(symbols.split(''))
+  }
+  // console.log(collection)
+  // remove things user do not need
+  if (options.excludeCharacters){
+    // console.log(`${options.excludeCharacters}`)
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+      // if the character includes in 'excludeCharacters',
+      // return false to remove the character in the collection
+      // if (options.excludeCharacters.includes(character) === true){
+      //   return false
+      // } else {
+      //   return true
+      // }
+    )
+    // console.log(collection)
+  }
+  // start generate password
+  let password = ''
+  for (let i = 0; i < Number(options.length); i++){
+    password += sample(collection)
+  }
+  // return the generated password
+  return password
+  // console.log('password:', password)
+  // console.log('This function will generate password.')
+}
+
+module.exports = generatePassword
+// generatePassword()
